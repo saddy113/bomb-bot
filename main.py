c469b16dfa73175e0f827e45eca78b08ce31b3dd
saddy <settawut.th@gmail.com>
2021-11-16 05:16:54 +0700

    init

---
+++ main.py
@@ -0,0 +1,215 @@
+# from PIL.ImageOps import grayscale
+
+import os
+import sys
+import time
+
+import pyautogui
+
+
+def main():
+    print('====START BOT BOMB====')
+
+    input_time = input('COUNT DOWN SETUP HERO (min): ')
+    hero_amount = input('HERO AMOUNT (number): ')
+    reset_position_time = input('RESET POSITION (min): ')
+
+    input_time = int(input_time)
+    hero_amount = int(hero_amount)
+    reset_position_time = int(reset_position_time)
+
+    # leve_time = count_down / 2
+
+    sec = 60
+    count_down = input_time * sec
+    init_time = input_time * sec
+
+    if reset_position_time > input_time:
+        sys.exit('reset position time must not be more than count down')
+
+    reset_position_time = reset_position_time * sec
+    start_game()
+
+    while True:
+        print('countdown sec: ', count_down)
+        count_down -= 1
+
+        connect_wallet_button = set_connect_wallet_button()
+        if connect_wallet_button is not None:
+            start_game()
+
+        if count_down > 1:
+            close_hunt_button = pyautogui.locateOnScreen(
+                im_path('back-main.png'), grayscale=True, confidence=.95)
+
+            new_map()
+
+            # start new round
+            if close_hunt_button is None:
+                hero_work(hero_amount)
+                treasure_hunt()
+
+            # back to main menu edit bug
+            if count_down == reset_position_time:
+                close_treasure_hunt()
+                treasure_hunt()
+
+        if count_down <= 1:
+            close_treasure_hunt()
+            count_down = init_time
+
+        time.sleep(1)
+
+
+def im_path(filename):
+    return os.path.join('images', filename)
+
+
+def set_connect_wallet_button():
+    connect_wallet_button = pyautogui.locateOnScreen(
+        im_path('connect-wallet.png'), grayscale=True, confidence=.8)
+
+    return connect_wallet_button
+
+
+def start_game():
+    print('===connect wallet===')
+
+    connect_wallet_button = set_connect_wallet_button()
+
+    if connect_wallet_button is None:
+        sys.exit("connect wallet button not found")
+
+    pyautogui.moveTo(connect_wallet_button, duration=0.5)
+    pyautogui.click(connect_wallet_button)
+
+    time.sleep(2)
+
+    print('===connect metamask===')
+    metamask = pyautogui.locateOnScreen(
+        im_path('metamask.png'), grayscale=True, confidence=.95)
+
+    if metamask is None:
+        sys.exit("metamask button not found")
+
+    pyautogui.moveTo(metamask, duration=0.5)
+    pyautogui.click(metamask)
+
+    time.sleep(3)
+
+    print('sing metamask')
+    sing = pyautogui.locateOnScreen(
+        im_path('sing.png'), grayscale=True, confidence=.95)
+
+    # print(sing)
+    if sing is None:
+        sys.exit("sing button not found")
+
+    pyautogui.moveTo(sing, duration=0.5)
+    pyautogui.click(sing)
+
+    # loop wait for loading
+    while True:
+        start_hunt = pyautogui.locateOnScreen(
+            im_path('start-game.png'), grayscale=True, confidence=.95)
+
+        if start_hunt is not None:
+            break
+
+        time.sleep(1)
+        print("wait for loading")
+
+
+def hero_work(hero_amount):
+    time.sleep(2)
+    print('====start hero to work====')
+
+    hero_button = pyautogui.locateOnScreen(
+        im_path('hero.png'), grayscale=True, confidence=.95)
+
+    # print(hero_button)
+    if hero_button is None:
+        sys.exit("hero button not found")
+
+    pyautogui.moveTo(hero_button, duration=0.5)
+    pyautogui.click(hero_button)
+    time.sleep(2)
+
+    while True:
+        work_button = pyautogui.locateOnScreen(
+            im_path('work-non-active.png'), grayscale=True, confidence=.95)
+
+        work_button_active = pyautogui.locateOnScreen(
+            im_path('work-active.png'), grayscale=True, confidence=.95)
+
+        if work_button is not None:
+            pyautogui.moveTo(work_button, duration=0.5)
+            pyautogui.drag(0, -500, duration=0.2)
+            time.sleep(1)
+            break
+
+        elif work_button_active:
+            pyautogui.moveTo(work_button_active, duration=0.5)
+            pyautogui.drag(0, -500, duration=0.2)
+            time.sleep(1)
+            break
+
+    work = None
+    for work in pyautogui.locateAllOnScreen(im_path('work-non-active.png'), grayscale=True, confidence=.95):
+        print('work not active')
+
+    time.sleep(1)
+
+    if work is not None:
+        for clickWork in range(hero_amount + 1):
+            pyautogui.click(work)
+            print("click hero not active: ", clickWork)
+            time.sleep(1)
+
+    close_button_hero = pyautogui.locateOnScreen(
+        im_path('close.png'), grayscale=True, confidence=.95)
+
+    pyautogui.moveTo(close_button_hero, duration=0.5)
+    pyautogui.click(close_button_hero)
+    time.sleep(1.5)
+
+
+def treasure_hunt():
+    print('===start treasure hunt===')
+    time.sleep(2)
+    start_hunt = pyautogui.locateOnScreen(
+        im_path('start-game.png'), grayscale=True, confidence=.95)
+
+    if start_hunt is None:
+        sys.exit("treasure hunt button not found")
+
+    pyautogui.moveTo(start_hunt, duration=0.5)
+    pyautogui.click(start_hunt)
+
+
+def close_treasure_hunt():
+    print('===close treasure hunt===')
+    close_hunt_button = pyautogui.locateOnScreen(
+        im_path('back-main.png'), grayscale=True, confidence=.95)
+
+    if close_hunt_button is None:
+        sys.exit("back to main menu button not found")
+
+    pyautogui.moveTo(close_hunt_button, duration=0.5)
+    pyautogui.click(close_hunt_button)
+    time.sleep(1.5)
+
+
+def new_map():
+    print('===click new map button===')
+    new_map_button = pyautogui.locateOnScreen(
+        im_path('new-map.png'), grayscale=True, confidence=.95)
+
+    if new_map_button is not None:
+        pyautogui.moveTo(new_map_button, duration=0.5)
+        pyautogui.click(new_map_button)
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    main()
